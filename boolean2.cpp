e286514f3f107f2ce09fc6970d257032a77bbe21
bahlil <raditradityarahman068@gmail.com>
2025-11-21 14:31:50 +0700

    Add files via upload

---
+++ boolean2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    var1 = 17 < 10;
+    var2 = 50 > 20
+    var3 = 'Y' == 'y'
+
+    cout << "hasilnya var1 =" << var1 << endl;
+    cout << "hasilnya var2 =" << var2 << endl;
+    cout << "hasilnya var3 =" << var3 << endl;
+}
